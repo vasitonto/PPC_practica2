2627612571642113884e5ae5f12674539bf31c9f
josemi <alcarazjosemi@gmail.com>
2024-11-17 13:39:56 +0100

    lanzador añadido

---
+++ PPC_practica2/src/pruebas/Lanzador.java
@@ -0,0 +1,15 @@
+package pruebas;
+import cliente.Client;
+import servidor.Server;
+
+public class Lanzador {
+	public static void main(String[] args) {
+		Server serv = new Server();
+		Client cliente = new Client();
+		
+		cliente.start();
+		serv.start();
+		
+		
+	}
+}
