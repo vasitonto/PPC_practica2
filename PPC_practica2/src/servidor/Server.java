a08a56c9c47452373cfa84c23e6c5add2c780f90
vasito <alcarazjosemi@gmail.com>
2024-11-23 04:11:10 +0100

    Servidor ahora envía broadcast

--- PPC_practica2/src/servidor/Server.java
+++ PPC_practica2/src/servidor/Server.java
@@ -5,6 +5,8 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
@@ -19,29 +21,30 @@ public class Server extends Thread{
 	//TODO hacer el sistema de forma que se puedan lanzar varios servidores
 	// es decir, que el nº del puerto vaya cambiando
 	private static final int BCSocketPort = 4445; // puerto desde el cual se enviarán los mensajes bcast
-	private static final int RCVPORT = 4446;
+//	private static final int RCVPORT = 4446;
 	private static final int BCPORT = 4999; // puerto en el que escucha el cliente
+	private static final String grupoMulticast = "224.48.75.1";
 	private InetSocketAddress BCADDR;
-	private DatagramSocket BCsocket;
+	private MulticastSocket BCsocket;
 	private DatagramSocket RCVsocket;
 	private byte[] buf = new byte[256];
 	
 	public Server(){ 
 		try {
-			BCsocket = new DatagramSocket(BCSocketPort);
-			BCsocket.setBroadcast(true);
+			this.BCsocket = new MulticastSocket(BCSocketPort);
 			// BCADDR será la dirección a la que se enviarán los paquetes de broadcast
-			this.BCADDR = new InetSocketAddress(InetAddress.getByName("127.0.0.255"), BCPORT);
+			this.BCADDR = new InetSocketAddress(InetAddress.getByName(grupoMulticast), BCPORT);
 			
-		} catch (SocketException | UnknownHostException e) {
+		} catch (IOException e) {
 //			System.err.println("Error en el establecimiento del puerto. Reintentando...\n");
 			e.printStackTrace();
 		}
 	}
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Server server = new Server();
+		server.BCsocket.joinGroup(server.BCADDR, NetworkInterface.getByName(grupoMulticast));
 		ServerQueryResponder responder = new ServerQueryResponder(server.BCsocket);
 		ServerBroadcaster broadcaster = new ServerBroadcaster(server.BCADDR, server.BCsocket);
 		responder.start();
